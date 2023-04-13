e5c753a0cca130875d7564f03dc0429433fc0229
egakipedia <juancarlos.roldan@egaclm.com>
2023-04-13 19:13:55 +0200

    Cambios Finales

--- app/src/main/java/com/example/scrollview/MainActivity.java
+++ app/src/main/java/com/example/scrollview/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.scrollview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+
+    public void Seleccion(View view){
+        switch (view.getId()){
+            case R.id.fresas:
+                Toast.makeText(this, "Estas son fresas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.manzanas:
+                Toast.makeText(this, "Estas son manzanas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.platanos:
+                Toast.makeText(this, "Estas son platanos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cerezas:
+                Toast.makeText(this, "Estas son cerezas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.frambuesas:
+                Toast.makeText(this, "Estas son frambuesas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.kiwis:
+                Toast.makeText(this, "Estas son kiwis", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mangos:
+                Toast.makeText(this, "Estas son mangos", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.melon:
+                Toast.makeText(this, "Estas son melon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.naranjas:
+                Toast.makeText(this, "Estas son naranjas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.peras:
+                Toast.makeText(this, "Estas son peras", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pina:
+                Toast.makeText(this, "Estas son piña", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sandia:
+                Toast.makeText(this, "Estas son sandia", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.uvas:
+                Toast.makeText(this, "Estas son uvas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.zarzamoras:
+                Toast.makeText(this, "Estas son zarzamoras", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
